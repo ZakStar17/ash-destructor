883fe0f884f8410a4cc28544ef65b8d04316064a
z17 <zzzzzstar17@gmail.com>
2024-12-25 00:06:21 -0100

    Removed Alloc from main trait declarations

--- src/lib.rs
+++ src/lib.rs
@@ -9,7 +9,7 @@ type Alloc<'a> = Option<&'a vk::AllocationCallbacks<'a>>;
 
 // can destroy itself using a device
 pub trait DeviceDestroyable {
-    unsafe fn destroy_self_alloc(&self, device: &ash::Device, allocation_callbacks: Alloc);
+    unsafe fn destroy_self_alloc(&self, device: &ash::Device, allocation_callbacks: Option<&vk::AllocationCallbacks>);
 
     unsafe fn destroy_self(&self, device: &ash::Device) {
         DeviceDestroyable::destroy_self_alloc(self, device, None);
@@ -18,7 +18,7 @@ pub trait DeviceDestroyable {
 
 // can destroy itself without the need of a device
 pub trait SelfDestroyable: DeviceDestroyable {
-    unsafe fn destroy_self_alloc(&self, allocation_callbacks: Alloc);
+    unsafe fn destroy_self_alloc(&self, allocation_callbacks: Option<&vk::AllocationCallbacks>);
 
     unsafe fn destroy_self(&self) {
         SelfDestroyable::destroy_self_alloc(self, None);
